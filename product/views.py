2010f796eb594b9dc25826adcc74c08c506b3c8b
code-masud <code.masud@hotmail.com>
2026-02-22 13:17:27 +0600

    url config

--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
+import asyncio
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Item
 
-# Create your views here.
+
+async def product_list(request):
+    data = [
+        item async for item in Item.objects.values("id", "name", "sku", "price", "category__name")
+    ]
+    # data = list(Item.objects.values('id', 'name', 'sku', 'price'))
+    return JsonResponse(data, safe=False)
