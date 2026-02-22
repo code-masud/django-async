bf9f42ff017313d8a6e4c34456dc5175fecc5ca5
code-masud <code.masud@hotmail.com>
2026-02-22 17:29:10 +0600

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'inventory'
 urlpatterns = [
+    path('inventory/', views.inventory_value, name='inventory_value'),
     path('product/', views.product_list, name='product_list'),
+    path('category/', views.category_summery, name='category_summery'),
+    path('movements/<int:pk>/', views.product_movements, name='product_movements'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
