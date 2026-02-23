5fc0bd8dcfb1b814bbd768786976ef9d6166051e
code-masud <code.masud@hotmail.com>
2026-02-23 15:28:26 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,14 @@ EMAIL_USE_TLS=os.getenv('EMAIL_USE_TLS', True)
 EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL=os.getenv('DEFAULT_FROM_EMAIL')
+
+
+CACHES = {
+    "default": {
+        "BACKEND": os.getenv('REDIS_BACKEND', "django_redis.cache.RedisCache"),
+        "LOCATION": os.getenv('REDIS_LOCATION', 'redis://127.0.0.1:6379'),
+        "OPTIONS": {
+            "CLIENT_CLASS": os.getenv('REDIS_CLIENT_CLASS', "django_redis.client.DefaultClient"),
+        }
+    }
+}
